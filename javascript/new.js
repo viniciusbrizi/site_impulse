e7cdf0884a21afeb4c8ea3deeeae9f5a53636479
Vinicius Brizi <brizivinicius@gmail.com>
2025-04-26 14:28:52 -0300

    first commit

---
+++ javascript/new.js
@@ -0,0 +1,9 @@
+// getElementById() / Trás apenas um elemento
+// getElementByClassNam() // Trás diversos elementos (HTML Collection)
+// getElementByTagName // Trás diversos elementps (HTML Collection)
+
+// - querySelector // Trás apenas um elemento, o primeiro que encontrar (element)
+// - querySelectpt / Trás todos elementos 
+
+const element = document.getElementById("main-text")
+console.log(element)
